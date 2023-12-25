d3a6ffc774ddf4ef7fdaf07bbaf4742ad83d7a88
Toan <92291488+QToan02@users.noreply.github.com>
2023-12-25 23:05:05 +0700

    Update ICard type for constant values

--- practice-two/app/constants/screens/checkout.ts
+++ practice-two/app/constants/screens/checkout.ts
@@ -1,26 +1,26 @@
-import { ICardInformation, IDetailInfo, IFlatListBase } from '@types'
+import { ICardBase, IDetailInfo, IFlatListBase } from '@types'
 
 const PRICE_DETAILS: IDetailInfo[] = [
   { id: 1, label: 'price (1 item)', value: '$25' },
   { id: 2, label: 'delivery fee', value: 'info' },
 ]
 
-export type ICardItem = IFlatListBase & ICardInformation
+export type ICardItem = IFlatListBase & ICardBase
 
 const CARDS: ICardItem[] = [
   {
     id: '1',
     name: 'Tradly Team',
-    cardNumber: '5627215898548869',
+    number: '5627215898548869',
     cvc: '111',
-    expires: '01/2019',
+    expired: '01/2019',
   },
   {
     id: '2',
     name: 'Tradly Team',
-    cardNumber: '4627215898548869',
+    number: '4627215898548869',
     cvc: '123',
-    expires: '12/2023',
+    expired: '12/2023',
   },
 ]
 
