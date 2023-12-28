97302956b680ebff3d59998daf912f81966318bb
Toan <92291488+QToan02@users.noreply.github.com>
2023-12-28 19:37:12 +0700

    Handle convert date time in TrackerItem component

--- practice-two/app/components/TrackerItem/index.tsx
+++ practice-two/app/components/TrackerItem/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react'
+import { memo, useMemo } from 'react'
 import isEqual from 'react-fast-compare'
 import { View, ViewProps } from 'react-native'
 
@@ -10,27 +10,37 @@ import styles from './styles'
 export interface TrackerItemProps extends ViewProps {
   trackStatus: TTrackerStatus
   description: string
-  date: Date
-  time: string
+  datetime: string
 }
 
-const TrackerItem = ({
-  trackStatus,
-  description,
-  date,
-  time,
-  style,
-  ...rest
-}: TrackerItemProps) => {
+const TrackerItem = ({ trackStatus, description, datetime, style, ...rest }: TrackerItemProps) => {
+  const convert = useMemo(() => {
+    if (!datetime) return {}
+    const convertDate = new Date(datetime)
+
+    const hour = convertDate.getUTCHours()
+    const minute = convertDate.getUTCMinutes()
+
+    let formattedHour = hour % 12
+    formattedHour = formattedHour === 0 ? 12 : formattedHour
+
+    const period = hour < 12 ? 'AM' : 'PM'
+
+    return {
+      date: convertDate.toLocaleDateString('en-GB'),
+      time: `${formattedHour}:${minute} ${period}`,
+    }
+  }, [datetime])
+
   return (
     <View style={[styles.container, style]} {...rest}>
       <View style={[containerStyles.inline, containerStyles.spaceBetween]}>
         <Paragraph content={trackStatus} style={[styles.text, styles.title]} />
-        <Paragraph content={date.toLocaleDateString('en-GB')} style={styles.text} />
+        <Paragraph content={convert.date || ''} style={styles.text} />
       </View>
       <View style={[containerStyles.inline, containerStyles.spaceBetween]}>
         <Paragraph content={description} style={styles.text} />
-        <Paragraph content={time} style={styles.text} />
+        <Paragraph content={convert.time || ''} style={styles.text} />
       </View>
     </View>
   )
