7febb92e84a3035bea9da6c0ad3962c6ec94c411
Toan <92291488+QToan02@users.noreply.github.com>
2023-12-28 19:40:11 +0700

    Update return type from BE

--- practice-two/app/services/common/index.ts
+++ practice-two/app/services/common/index.ts
@@ -1,42 +1,51 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import { IReturnResponse } from '@types'
 
 const request: AxiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  // baseURL: process.env.BASE_URL,
+  baseURL: 'http://10.0.2.2:5050',
 })
 
-export const get = async <T>(path: string, options: AxiosRequestConfig = {}): Promise<T[]> => {
-  const res = await request.get(path, options).catch((error) => {
+export const get = async <T>(path: string, options: AxiosRequestConfig = {}): Promise<T> => {
+  const res: AxiosResponse<IReturnResponse<T>> = await request.get(path, options).catch((error) => {
     throw error
   })
 
-  return res.data
+  return res.data.data
 }
 
 export const find = async <T>(path: string, options: AxiosRequestConfig = {}): Promise<T> => {
-  const res = await request.get(path, options).catch((error) => {
+  const res: AxiosResponse<IReturnResponse<T>> = await request.get(path, options).catch((error) => {
     throw error
   })
 
-  return res.data
+  return res.data.data
 }
 
 export const add = async <T>(path: string, options: Partial<T>): Promise<T> => {
-  const res = await request.post(path, options).catch((error) => {
-    throw error
-  })
+  const res: AxiosResponse<IReturnResponse<T>> = await request
+    .post(path, options)
+    .catch((error) => {
+      throw error
+    })
 
-  return res.data
+  return res.data.data
 }
 
 export const edit = async <T>(path: string, id: number, options: Partial<T>): Promise<T> => {
-  const res = await request.patch(`${path}/${id}`, options).catch((error) => {
-    throw error
-  })
+  const res: AxiosResponse<IReturnResponse<T>> = await request
+    .patch(`${path}/${id}`, options)
+    .catch((error) => {
+      throw error
+    })
 
-  return res.data
+  return res.data.data
 }
 
-export const remove = async (path: string, id: number): Promise<AxiosResponse['status']> => {
+export const remove = async (
+  path: string,
+  id: number | string
+): Promise<AxiosResponse['status']> => {
   const res = await request.delete(`${path}/${id}`).catch((error) => {
     throw error
   })
